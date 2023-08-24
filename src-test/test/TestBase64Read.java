197bb144ffc815fefdd53145112a3acd7a40350a
Joan Costa <joan.costa.mombiela@gmail.com>
2023-08-24 12:28:15 +0200

    Se elimina clase antigua

--- src-test/test/TestBase64Read.java
+++ src-test/test/TestBase64Read.java
@@ -2,16 +2,17 @@ package test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Base64;
 
-import info.semantictext.utils.Base64;
 import info.semantictext.utils.FileUtils;
 
 public class TestBase64Read 
 {
     public static void main(String[] args) throws IOException {
 	String text = new String(FileUtils.readFile(new File("examples/foto.base64.txt")));
-	byte[] fotoBytes = Base64.decode(text);
-	FileUtils.saveByteArrayToFile(new File("examples/foto.gif"), fotoBytes);
 	System.out.println(text);
+	byte[] fotoBytes = Base64.getDecoder().decode(text);
+	System.out.println(fotoBytes.length);
+	FileUtils.saveByteArrayToFile(new File("examples/foto.gif"), fotoBytes);
     }
 }
