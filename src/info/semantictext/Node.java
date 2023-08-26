5d0de449155bfd2330c203cb41c08553ad30cc37
Joan Costa <joan.costa.mombiela@gmail.com>
2023-08-26 19:58:23 +0200

    I18N++

--- src/info/semantictext/Node.java
+++ src/info/semantictext/Node.java
@@ -8,7 +8,7 @@ public class Node
     // -------------------------------------
     // Basic properties, getters and setters
     // -------------------------------------
-    
+
     private String name;
     private String canonicalName;
     private String namespace;
@@ -16,79 +16,92 @@ public class Node
     private String value;
     private List<Node> childs;
     private int lineCreation;
-    
+
     public Type getType()
     {
         return type;
     }
+
     public void setType(Type type)
     {
         this.type = type;
     }
+
     public String getValue()
     {
         return value;
     }
+
     public String getTvalue()
     {
         return value != null ? value.trim() : "";
     }
+
     public void setValue(String value)
     {
         this.value = value;
     }
+
     public List<Node> getChilds()
     {
         return childs;
     }
+
     public void setChilds(List<Node> childs)
     {
         this.childs = childs;
-    }    
+    }
+
     public String getName()
     {
         return name;
     }
+
     public void setName(String name)
     {
         this.name = name;
     }
+
     public String getNamespace()
     {
         return namespace;
     }
+
     public void setNamespace(String namespace)
     {
         this.namespace = namespace;
-    }    
+    }
+
     public String getCanonicalName()
     {
         return canonicalName;
     }
+
     public void setCanonicalName(String canonicalName)
     {
         this.canonicalName = canonicalName;
     }
-    
+
     @Override
     public String toString()
     {
-        return  toString(1);
+        return toString(1);
     }
-    
+
     public int getLineCreation()
     {
         return lineCreation;
     }
+
     public void setLineCreation(int lineCreation)
     {
         this.lineCreation = lineCreation;
     }
-    
+
     // ----------------
     // ToString methods
     // ----------------
-    
+
     private String toString(int level)
     {
         StringBuffer result = new StringBuffer();
@@ -101,27 +114,27 @@ public class Node
         {
             result.append("\n");
             int numNode = 0;
-            for (int j = 0; j<childs.size(); j++)
+            for (int j = 0; j < childs.size(); j++)
             {
-                for (int i = 0; i<level; i++) result.append("\t");
+                for (int i = 0; i < level; i++) result.append("\t");
                 Node c = childs.get(j);
-                result.append("[" + numNode +"]" + c.toString(level+1));
-                if (j!=childs.size()-1) result.append("\n");
+                result.append("[" + numNode + "]" + c.toString(level + 1));
+                if (j != childs.size() - 1) result.append("\n");
                 numNode++;
             }
         }
-        return  result.toString();
-    }    
-    
+        return result.toString();
+    }
+
     public String toSTXT()
     {
         return toSTXT(1);
     }
-    
+
     private String toSTXT(int level)
     {
         StringBuffer result = new StringBuffer();
-        result.append(canonicalName + (level==1? "(" + namespace + ")" : "") + ":");
+        result.append(canonicalName + (level == 1 ? "(" + namespace + ")" : "") + ":");
         if (type != Type.NODE)
         {
             result.append(tabulatedText(value, level));
@@ -129,37 +142,37 @@ public class Node
         if (childs != null)
         {
             result.append("\n");
-            for (int j = 0; j<childs.size(); j++)
+            for (int j = 0; j < childs.size(); j++)
             {
-                for (int i = 0; i<level; i++) result.append("\t");
+                for (int i = 0; i < level; i++) result.append("\t");
                 Node c = childs.get(j);
-                result.append(c.toSTXT(level+1));
-                if (j!=childs.size()-1) result.append("\n");
+                result.append(c.toSTXT(level + 1));
+                if (j != childs.size() - 1) result.append("\n");
             }
         }
-        return  result.toString();
+        return result.toString();
     }
-    
+
     private String tabulatedText(String text, int level)
     {
-        if (text.indexOf('\n')==-1) return text;
-        
+        if (text.indexOf('\n') == -1) return text;
+
         StringBuffer result = new StringBuffer();
-        for (int i = 0; i<level; i++) result.append("\t");
-        
+        for (int i = 0; i < level; i++) result.append("\t");
+
         text = text.replaceAll("\\n", '\n' + result.toString());
         return text;
     }
-    
+
     public String toSTXTCompact()
     {
         return toSTXTCompact(1);
     }
-    
+
     private String toSTXTCompact(int level)
     {
         StringBuffer result = new StringBuffer();
-        result.append(canonicalName + (level==1? "(" + namespace + ")" : "") + ":");
+        result.append(canonicalName + (level == 1 ? "(" + namespace + ")" : "") + ":");
         if (type != Type.NODE)
         {
             result.append(compactTabulatedText(value, level));
@@ -167,43 +180,43 @@ public class Node
         if (childs != null)
         {
             result.append("\n");
-            for (int j = 0; j<childs.size(); j++)
+            for (int j = 0; j < childs.size(); j++)
             {
                 result.append(level + ":");
                 Node c = childs.get(j);
-                result.append(c.toSTXTCompact(level+1));
-                if (j!=childs.size()-1) result.append("\n");
+                result.append(c.toSTXTCompact(level + 1));
+                if (j != childs.size() - 1) result.append("\n");
             }
         }
-        return  result.toString();
+        return result.toString();
     }
-    
+
     private String compactTabulatedText(String text, int level)
     {
-        if (text.indexOf('\n')==-1) return text;
+        if (text.indexOf('\n') == -1) return text;
         if (type != Type.TEXT) text = text.trim();
         text = text.replaceAll("\\n", '\n' + Integer.toString(level) + ':');
         return text;
     }
 
-    // M�todos de acceso r�pido a hijos
+    // Fast access methods to children
     public List<Node> getChilds(String cname)
     {
         List<Node> result = new ArrayList<Node>();
-        
+
         for (Node child: childs)
         {
             if (child.getCanonicalName().equalsIgnoreCase(cname)) result.add(child);
         }
-        
+
         return result;
     }
-    
+
     public Node getChild(String cname)
     {
         List<Node> result = getChilds(cname);
-        if (result.size()>1) throw new IllegalArgumentException("More than 1 child. Use getChilds");
-        if (result.size()==0) return null;
+        if (result.size() > 1) throw new IllegalArgumentException("More than 1 child. Use getChilds");
+        if (result.size() == 0) return null;
         return result.get(0);
     }
 }
