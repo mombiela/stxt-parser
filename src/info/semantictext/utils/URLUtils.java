b69fd868e086d27be784a2a7cc2f99a3535733a5
Joan Costa Mombiela <jcostamo@fiatc.es>
2024-06-03 08:20:11 +0200

    M+

--- src/info/semantictext/utils/URLUtils.java
+++ src/info/semantictext/utils/URLUtils.java
@@ -3,18 +3,27 @@ package info.semantictext.utils;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class URLUtils {
-    public static String getUrlContent(URL url) throws IOException
-    {
-        InputStream in = url.openStream();
-        InputStreamReader reader = new InputStreamReader(in, Constants.ENCODING);
+    public static String getUrlContent(URL url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+        connection.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+        connection.setRequestProperty("Accept", "text/plain");
 
-        StringBuilder sb = new StringBuilder(Math.max(16, in.available()));
-        char[] tmp = new char[4096];
-        for(int cnt; (cnt = reader.read(tmp)) > 0; ) sb.append(tmp, 0, cnt);
-        
+        InputStream in = connection.getInputStream();
+        InputStreamReader reader = new InputStreamReader(in, "UTF-8");
+
+        StringBuilder sb = new StringBuilder();
+        char[] buffer = new char[4096];
+        int length;
+        while ((length = reader.read(buffer)) != -1) {
+            sb.append(buffer, 0, length);
+        }
+
+        reader.close();
         return sb.toString();
     }
     
