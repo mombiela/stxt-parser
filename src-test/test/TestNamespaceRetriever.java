44463aa5c00d30d614f41657e225f79ddec4b7b0
Joan Costa Mombiela <jcostamo@fiatc.es>
2024-06-13 18:36:55 +0200

    M+

---
+++ src-test/test/TestNamespaceRetriever.java
@@ -0,0 +1,25 @@
+package test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
+
+import info.NamespaceRetriever;
+import info.ParseException;
+
+public class TestNamespaceRetriever extends AbstractTestParser
+{
+    public static void main(String[] args) throws IOException, ParseException
+    {
+        NamespaceRetriever nsRetriver = new NamespaceRetriever();
+        nsRetriver.addGrammarDefinitionsFromDir(new File("defs"));
+        Set<String> namespaces = nsRetriver.getAllNamespaces();
+        
+        for (String namespace: namespaces)
+        {
+            System.out.println("*************************************************************");
+            System.out.println(namespace);
+            System.out.println(nsRetriver.getNameSpace(namespace));
+        }
+    }    
+}
--- src/info/NamespaceRetriever.java
+++ src/info/NamespaceRetriever.java
@@ -4,33 +4,67 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class NamespaceRetriever
 {
-    private static Map<String, String> CACHE = new HashMap<>();
+    private Map<String, Namespace> CACHE = new HashMap<>();
     
-    public void addGrammarDefinition(String nameSpace, String content)
+    public void addGrammarDefinition(String content) throws IOException, ParseException
     {
-        CACHE.put(nameSpace, content);
+        addGrammarDefinition(content, null);
     }
     
-    public static void addGrammarDefinitionsFromDir(File dir) throws IOException
+    public Set<String> getAllNamespaces()
+    {
+        return new HashSet(CACHE.keySet());
+    }
+    
+    private void addGrammarDefinition(String content, String expected) throws IOException, ParseException
+    {
+        // Parser content
+        Parser parser = new Parser();
+        NamespaceProcessor processor = new NamespaceProcessor();
+        parser.addNodeProcessor(processor);
+        parser.parse(content);
+        
+        // Get result
+        List<Namespace> namespaces = processor.getNamespaces();
+        
+        if (expected != null && (namespaces.size()!= 1 || !namespaces.get(0).getName().equals(expected)))
+            throw new ParseException("Namespace is " + namespaces.get(0).getName() + ", expected: " + expected, 0);
+        
+        // Add result
+        for (Namespace ns: namespaces)
+        {
+            String name = ns.getName();
+            if (CACHE.containsKey(name)) throw new ParseException("Namespace already exist: " + name, 0);
+            CACHE.put(name, ns);
+        }
+    }      
+    
+    public void addGrammarDefinitionsFromDir(File dir) throws IOException, ParseException
     {
         // Read definitions from directory
         List<File> files = UtilsFile.getStxtFiles(dir);
         for (File f: files)
         {
-            //System.out.println("Grammar in file: " + f);
-            String name = f.getAbsolutePath().substring(dir.getAbsolutePath().length()+1);
-            name = name.replaceAll("\\\\", "/");
-            System.out.println("CACHE grammar: " + name);
-            CACHE.put(name, UtilsFile.readFileContent(f));
+            try
+            {
+                addGrammarDefinition(UtilsFile.readFileContent(f));
+            }
+            catch (Exception e)
+            {
+                System.err.println("Error processing: " + f.getAbsolutePath() + " -> " + e.getMessage());
+                throw e;
+            }
         }
     }
     
-    public static String getNameSpaceContent(String namespace) throws IOException
+    public Namespace getNameSpace(String namespace) throws IOException, ParseException
     {
         if (CACHE.containsKey(namespace)) return CACHE.get(namespace);
 	
@@ -38,7 +72,7 @@ public class NamespaceRetriever
         URL uri = new URL("https://" + namespace);
         String fileContent = Utils.getUrlContent(uri);
         
-        CACHE.put(namespace, fileContent);
+        addGrammarDefinition(fileContent, namespace);
 
         return CACHE.get(namespace);
     }
