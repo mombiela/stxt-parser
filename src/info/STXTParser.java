9d244daba85bf9ae327788762e98ad5d72349691
Joan Costa Mombiela <jcostamo@fiatc.es>
2024-06-14 08:23:40 +0200

    M+

--- src/info/STXTParser.java
+++ src/info/STXTParser.java
@@ -32,14 +32,23 @@ class STXTProcessor extends NodeBasicProcessor
     @Override
     public void processNodeOnCreation(Node node) throws ParseException, IOException
     {
-        if (debug) System.out.println(".... Node creation: <" + node.getName() + "> stack: " + node.getLevelCreation());
         if (node.getLevelCreation() == 0) updateMainNamespace(node);
     }
 
     @Override
-    public void processBeforeAdd(Node parent, Node child)
+    public void processBeforeAdd(Node parent, Node child) throws IOException, ParseException
     {
-        if (debug) System.out.println(".... Before add " + child.getName() + " to " + parent.getName());
+        System.out.println("**** " + child.getName() + " -> " + parent.getName());
+        
+        // Get namespace parent
+        String parentNamespace = (String) parent.getMetadata(NAMESPACE);
+        String parentName = parent.getName();
+        
+        NamespaceNode node = namespaceRetriever.getNameSpace(parentNamespace).getNode(parentName);
+        
+        // Check child name exist
+        if (!node.getChilds().containsKey(child.getName()))
+            throw new ParseException("Name not valid: " + child.getName(), child.getLineCreation());
     }
 
     // ------------
@@ -56,10 +65,13 @@ class STXTProcessor extends NodeBasicProcessor
         // Get namespace
         Namespace ns = namespaceRetriever.getNameSpace(namespace);
         if (ns == null) throw new ParseException("Namespace unknown: " + namespace, node.getLineCreation());
-        node.setMetadata(NAMESPACE, namespace);
         
         // Check exist name
         if (!ns.getNodes().containsKey(name))
             throw new ParseException("Name " + name + " not found in namespace " + namespace, node.getLineCreation());
+        
+        // Cambiamos nombre
+        node.setName(name);
+        node.setMetadata(NAMESPACE, namespace);
     }
 }
