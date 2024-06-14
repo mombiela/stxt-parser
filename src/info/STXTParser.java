ddbf041758664f17ed480f245ae2e62359d5c722
Joan Costa Mombiela <jcostamo@fiatc.es>
2024-06-14 08:13:09 +0200

    M+

--- src/info/STXTParser.java
+++ src/info/STXTParser.java
@@ -1,13 +1,65 @@
 package info;
 
+import java.io.IOException;
+
 public class STXTParser extends Parser
 {
-    private NamespaceRetriever namespaceRetriever;
-    
     public STXTParser(NamespaceRetriever namespaceRetriever)
     {
-        this.namespaceRetriever = namespaceRetriever;
         addNodeProcessor(new STXTProcessor(namespaceRetriever));
     }    
 }
 
+class STXTProcessor extends NodeBasicProcessor
+{
+    private static final String NAMESPACE = "namespace";
+    
+    // ----------------------------------
+    // Configuration and main constructor
+    // ----------------------------------
+    
+    private NamespaceRetriever namespaceRetriever;
+
+    public STXTProcessor(NamespaceRetriever namespaceRetriever)
+    {
+        this.namespaceRetriever = namespaceRetriever;
+    }
+
+    // ---------------
+    // Process methods
+    // ---------------
+    
+    @Override
+    public void processNodeOnCreation(Node node) throws ParseException, IOException
+    {
+        if (debug) System.out.println(".... Node creation: <" + node.getName() + "> stack: " + node.getLevelCreation());
+        if (node.getLevelCreation() == 0) updateMainNamespace(node);
+    }
+
+    @Override
+    public void processBeforeAdd(Node parent, Node child)
+    {
+        if (debug) System.out.println(".... Before add " + child.getName() + " to " + parent.getName());
+    }
+
+    // ------------
+    // Main methods
+    // ------------
+    
+    private void updateMainNamespace(Node node) throws IOException, ParseException
+    {
+        System.out.println("Updating main namespace: " + node.getName());
+        LineSplitter split = LineSplitter.split(node.getName());
+        String name = split.centralText;
+        String namespace = split.suffix;
+        
+        // Get namespace
+        Namespace ns = namespaceRetriever.getNameSpace(namespace);
+        if (ns == null) throw new ParseException("Namespace unknown: " + namespace, node.getLineCreation());
+        node.setMetadata(NAMESPACE, namespace);
+        
+        // Check exist name
+        if (!ns.getNodes().containsKey(name))
+            throw new ParseException("Name " + name + " not found in namespace " + namespace, node.getLineCreation());
+    }
+}
