5e1fac0244667bbe33523ed61c2c9c2fd78ac2fc
Joan Costa Mombiela <jcostamo@fiatc.es>
2024-06-14 09:47:38 +0200

    M+

--- src/info/ParseException.java
+++ src/info/ParseException.java
@@ -4,8 +4,11 @@ public class ParseException extends Exception
 {
     private static final long serialVersionUID = 1L;
 
+    public int line;
+    
     public ParseException(String message, int line) 
     {
         super("Line " + line + ": " + message);
+        this.line = line;
     }
 }
