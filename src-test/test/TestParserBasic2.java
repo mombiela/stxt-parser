d4b80ae61b44e28fd5d49e0f2156a2bfaa38c98c
Joan Costa <joan.costa.mombiela@gmail.com>
2024-06-15 22:02:06 +0200

    M+

---
+++ src-test/test/TestParserBasic2.java
@@ -0,0 +1,58 @@
+package test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+import info.semantictext.BasicProcessor;
+import info.semantictext.Node;
+import info.semantictext.ParseException;
+import info.semantictext.Parser;
+
+public class TestParserBasic2
+{
+    public static void main(String[] args) throws IOException, ParseException
+    {
+        new TestParserBasic2().mainTest();
+    }
+    
+    @Test
+    public void mainTest() throws IOException, ParseException
+    {
+        System.out.println("Inici");
+
+        Set<String> multilineNodes = new HashSet<>();
+        multilineNodes.add("contenido");
+        multilineNodes.add("cuerpo");
+        multilineNodes.add("Foto");
+        
+        // Create parser
+        Parser parser = createBasicParser(multilineNodes);
+        
+        File f = new File("docs2/client2.stxt");
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println(f.getAbsolutePath());
+        List<Node> docs = parser.parseFile(f);
+        Node n = docs.get(0);
+        System.out.println(n);
+        
+        System.out.println("End");
+    }
+
+    private Parser createBasicParser(Set<String> multilineNodes)
+    {
+        Parser parser = new Parser();
+        BasicProcessor basicProcessor = new BasicProcessor();
+        basicProcessor.setMultilineNodes(multilineNodes);
+        parser.addNodeProcessor(basicProcessor);
+        
+        return parser;
+    }
+
+}
