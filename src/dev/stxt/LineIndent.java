c90b07a78a2a03eda3139087f84a1931bc997550
Joan Costa <jcostamo@fiatc.es>
2025-12-02 12:09:45 +0100

    M+

---
+++ src/dev/stxt/LineIndent.java
@@ -0,0 +1,96 @@
+package dev.stxt;
+
+import java.util.regex.Pattern;
+
+import info.semantictext.parser.Constants;
+import info.semantictext.parser.ParseException;
+
+public class LineIndent
+{
+    public final int indentLevel;
+    public final String lineWithoutIndent;
+    
+    private LineIndent(int level, String line)
+    {
+        this.indentLevel = level;
+        this.lineWithoutIndent = line;
+    }
+    
+    // ---------
+    // Constants
+    // ---------
+
+    // Patterns
+    private static final Pattern EMPTY_LINE = Pattern.compile("^\\s*$");
+    private static final Pattern COMMENT_LINE = Pattern.compile("^\\s*\\#.*$");
+    private static final Pattern COMPACT_LINE = Pattern.compile("^\\d+");
+    
+    public static LineIndent parseLine(String aLine, int numLine, ParseState parseState) throws ParseException
+    {
+        int stackSize = parseState.getStack().size();
+        boolean lastNodeMultiline = stackSize > 0 && NodeType.isMultiline(parseState.stackPeek().getType());
+        
+        // Validate if empty line or comment
+        if (!lastNodeMultiline)
+        {
+            if (EMPTY_LINE.matcher(aLine).matches() || COMMENT_LINE.matcher(aLine).matches())
+            {
+                return null;
+            }
+        }
+
+        // Compact line
+        if (COMPACT_LINE.matcher(aLine).find())
+        {
+            int i = aLine.indexOf(':');
+            return new LineIndent(Integer.parseInt(aLine.substring(0,i)), aLine.substring(i+1));
+        }
+
+        // Obtain the level and pointer
+        int level = 0;
+        int spaces = 0;
+
+        int pointer = 0;
+        while (pointer < aLine.length()) 
+        {
+            // Current (last) char
+            char charPointer = aLine.charAt(pointer);
+
+            // Update level
+            if (charPointer == Constants.SPACE) 
+            {
+                spaces++;
+                if (spaces == Constants.TAB_SPACES) 
+                {
+                    level++;
+                    spaces = 0;
+                }
+            } 
+            else if (charPointer == Constants.TAB) 
+            {
+                level++;
+                spaces = 0;
+            }
+            else 
+            {
+                break;
+            }
+
+            // Pointer position
+            pointer++;
+
+            // Validate that text can only have one more level, so no information is lost
+            if (lastNodeMultiline && level >= stackSize) break;
+        }
+        if (spaces != 0) throw new ParseException("Invalid number spaces", numLine);
+
+        // In case of text, check if it's a comment or not to preserve empty line (depends on the comment's level)
+        if (lastNodeMultiline && level < stackSize)
+        {
+            if (EMPTY_LINE.matcher(aLine).matches())    return new LineIndent(stackSize, ""); // Preserve empty line
+            if (COMMENT_LINE.matcher(aLine).matches())  return null; // It's a comment
+        }
+
+        return new LineIndent(level, aLine.substring(pointer));
+    }
+}
